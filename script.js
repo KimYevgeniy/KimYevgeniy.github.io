a18a41c88fe42461c5dfbebf29361b7ddbcc60c3
Yevgeniy Kim <kimeval@gmail.com>
2023-11-15 21:47:03 +0600

    feat: overlay, bug fixes

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-// script.js
-const follower = document.getElementById("follower");
+// //follower script.js
+// const follower = document.getElementById("follower");
 
-// Function to update the follower div position
-function updateFollowerPosition(event) {
-  const mouseX = event.clientX;
-  const mouseY = event.clientY;
+// // Function to update the follower div position
+// function updateFollowerPosition(event) {
+//   const mouseX = event.clientX;
+//   const mouseY = event.clientY;
 
-  const scrollX = window.scrollX || window.pageXOffset;
-  const scrollY = window.scrollY || window.pageYOffset;
+//   const scrollX = window.scrollX || window.pageXOffset;
+//   const scrollY = window.scrollY || window.pageYOffset;
 
-  const pageWidth = document.documentElement.clientWidth;
-  const pageHeight = document.documentElement.clientHeight;
+//   const pageWidth = document.documentElement.clientWidth;
+//   const pageHeight = document.documentElement.clientHeight;
 
-  const followerWidth = follower.clientWidth;
-  const followerHeight = follower.clientHeight;
+//   const followerWidth = follower.clientWidth;
+//   const followerHeight = follower.clientHeight;
 
-  // Calculate the maximum allowed position for the follower div
-  const maxX = pageWidth - followerWidth;
-  const maxY = pageHeight - followerHeight + 400;
+//   // Calculate the maximum allowed position for the follower div
+//   const maxX = pageWidth - followerWidth;
+//   const maxY = pageHeight - followerHeight + 400;
 
-  // Calculate the new position while considering scroll and boundaries
-  let newLeft = mouseX + scrollX;
-  let newTop = mouseY + scrollY;
+//   // Calculate the new position while considering scroll and boundaries
+//   let newLeft = mouseX + scrollX;
+//   let newTop = mouseY + scrollY;
 
-  // Ensure the follower div stays within the page boundaries
-  newLeft = Math.min(Math.max(newLeft, 0), maxX);
-  newTop = Math.min(Math.max(newTop, 0), maxY);
+//   // Ensure the follower div stays within the page boundaries
+//   newLeft = Math.min(Math.max(newLeft, 0), maxX);
+//   newTop = Math.min(Math.max(newTop, 0), maxY);
 
-  // Update the position of the follower div
-  follower.style.left = newLeft + "px";
-  follower.style.top = newTop + "px";
-}
+//   // Update the position of the follower div
+//   follower.style.left = newLeft + "px";
+//   follower.style.top = newTop + "px";
+// }
 
-document.addEventListener("mousemove", updateFollowerPosition);
-window.addEventListener("scroll", updateFollowerPosition);
-window.addEventListener("resize", updateFollowerPosition);
+// document.addEventListener("mousemove", updateFollowerPosition);
+// window.addEventListener("scroll", updateFollowerPosition);
+// window.addEventListener("resize", updateFollowerPosition);
